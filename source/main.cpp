8fa731746cda35dd384ec9153ee942b4a3d69905
Jetstream <bigalex934@gmail.com>
2022-07-29 23:29:23 +0300

    Implement splitting source file in chunk files with lesser size

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,7 @@
+#include <iostream>
 #include <algorithm>
-#include <chrono>
 #include <filesystem>
 #include <fstream>
-#include <iostream>
 #include <iterator>
 #include <vector>
 
@@ -35,59 +34,50 @@ void merge(InputIterator first1, InputIterator last1, InputIterator first2, Inpu
           std::forward<OutputIterator>(out), std::greater<typename InputIterator::value_type>());
 }
 
+template<typename It>
+std::vector<std::size_t> get_chunk(It& forwarder, std::size_t bytes_in_chunk) {
+    std::vector<std::size_t> chunk_data;
+    It end;
+    for (std::size_t bytes_read = 0; forwarder != end && bytes_read < bytes_in_chunk;) {
+        chunk_data.push_back(*forwarder);
+        const auto delim_bytes = 1;
+        bytes_read += std::to_string(*forwarder).size() + delim_bytes;
+        forwarder = std::next(forwarder);
+    }
+    return std::move(chunk_data);
+}
+
+void split_source_in_chunks(const std::filesystem::path& path, std::size_t bytes_in_chunk) {
+
+    if (bytes_in_chunk <= 0) {
+        throw std::runtime_error("Bad chunk size");
+    }
+
+    std::fstream file(path, std::fstream::in);
+    if (!file.is_open()) {
+        throw std::runtime_error("Cannot open source file");
+    }
+
+    const std::filesystem::path temp_directory = "tmp/";
+    std::filesystem::create_directory(temp_directory);
+
+    std::istream_iterator<std::size_t> forwarder(file);
+    std::istream_iterator<std::size_t> end;
+    for (std::size_t chunk_index = 0;forwarder != end; ++chunk_index) {
+        auto next_chunk = get_chunk(forwarder, bytes_in_chunk);
+        const std::filesystem::path chunk_file_name = temp_directory.string() + std::to_string(chunk_index);
+        std::fstream chunk_file(chunk_file_name, std::fstream::out);
+        std::ostream_iterator<std::size_t> chunk_file_iterator(chunk_file, " ");
+        std::move(next_chunk.begin(), next_chunk.end(), chunk_file_iterator);
+    }
+
+    std::filesystem::remove_all(temp_directory);
+}
+
 }
 
 
 int main(int argc, char *argv[]) {
-    std::vector<int> part1 = {10, 23, 4, 2, 14};
-    std::vector<int> part2 = {59, 74, 23, 12};
-    std::vector<int> merger;
-    custom::merge(part1.begin(), part1.end(), part2.begin(), part2.end(), std::back_inserter(merger));
-
-    //    std::filesystem::path resources = "resources/";
-    //
-    //    std::string source_filename = "data.txt";
-    //    std::filesystem::path source = resources.string() + source_filename;
-    //
-    //    std::fstream file(source.c_str(), std::ios::in);
-    //    if (!file.is_open()) {
-    //        std::cerr << "Cannot open file for reading data" << std::endl;
-    //        return 1;
-    //    }
-    //    std::istream_iterator<int> source_fwd(file);
-    //    std::istream_iterator<int> source_end;
-    //
-    //    std::vector<int> buffer;
-    //
-    //    std::cout << "Start copy" << std::endl;
-    //    const auto start_reading_time = std::chrono::system_clock::now().time_since_epoch();
-    //    std::copy(source_fwd, source_end, std::back_inserter(buffer));
-    //    const auto end_reading_time = std::chrono::system_clock::now().time_since_epoch();
-    //    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::seconds>(end_reading_time - start_reading_time).count() << std::endl;
-    //    std::cout << "Done" << std::endl;
-    //
-    //    std::cout << "Start sorting" << std::endl;
-    //    const auto start_sorting_time = std::chrono::system_clock::now().time_since_epoch();
-    //    std::sort(buffer.begin(), buffer.end());
-    //    const auto end_sorting_time = std::chrono::system_clock::now().time_since_epoch();
-    //    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::seconds>(end_sorting_time - start_sorting_time).count() << std::endl;
-    //    std::cout << "Done" << std::endl;
-    //
-    //    std::string out_filename = "out.txt";
-    //    std::filesystem::path out = resources.string() + out_filename;
-    //    std::fstream out_file(out, std::ios::out | std::ios::trunc);
-    //    if (!out_file.is_open()) {
-    //        std::cerr << "Cannot open file for writing sorted data" << std::endl;
-    //        return 1;
-    //    }
-    //
-    //    std::cout << "Start saving" << std::endl;
-    //    const auto start_writing_time = std::chrono::system_clock::now().time_since_epoch();
-    //    std::ostream_iterator<int> out_fwd(out_file, " ");
-    //    std::copy(buffer.begin(), buffer.end(), out_fwd);
-    //    const auto end_writing_time = std::chrono::system_clock::now().time_since_epoch();
-    //    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::seconds>(end_writing_time - start_writing_time).count() << std::endl;
-    //    std::cout << "Done" << std::endl;
-
+    custom::split_source_in_chunks("resources/data.txt", 1);
     return 0;
 }

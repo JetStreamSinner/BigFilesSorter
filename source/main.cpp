ac20d42791cfcd7015d1bff9fa79f3107feda375
Jetstream <bigalex934@gmail.com>
2022-08-02 21:22:03 +0300

    Replace chunk info with std::string

--- source/main.cpp
+++ source/main.cpp
@@ -7,13 +7,6 @@
 
 namespace custom {
 
-    struct ChunksInfo {
-        std::string chunk_name;
-        template<typename T>
-        explicit ChunksInfo(const T &chunk_name) : chunk_name(chunk_name) {
-        }
-    };
-
     template<typename InputIterator, typename OutputIterator, typename Comparator>
     void merge(InputIterator first1, InputIterator last1, InputIterator first2, InputIterator last2, OutputIterator out, Comparator predicate) {
         while (first1 != last1 && first2 != last2) {
@@ -54,7 +47,7 @@ namespace custom {
         return std::move(chunk_data);
     }
 
-    std::vector<ChunksInfo> split_source_in_chunks(const std::filesystem::path &path, std::size_t bytes_in_chunk) {
+    std::vector<std::string> split_source_in_chunks(const std::filesystem::path &path, std::size_t bytes_in_chunk) {
         if (bytes_in_chunk <= 0) {
             throw std::runtime_error("Bad chunk size");
         }
@@ -71,7 +64,7 @@ namespace custom {
         std::istream_iterator<std::size_t> end;
 
         // Using structure because algorithm may need more fields in the future, so using only std::string isn't enough
-        std::vector<ChunksInfo> chunks_meta;
+        std::vector<std::string> chunks_meta;
         for (std::size_t chunk_index = 0; forwarder != end; ++chunk_index) {
             auto next_chunk = get_chunk(forwarder, bytes_in_chunk);
             const std::filesystem::path chunk_file_name = temp_directory.string() + std::to_string(chunk_index);
@@ -81,11 +74,11 @@ namespace custom {
             std::move(next_chunk.begin(), next_chunk.end(), chunk_file_iterator);
             chunks_meta.emplace_back(chunk_file_name);
         }
-        //    std::filesystem::remove_all(temp_directory);
+        // std::filesystem::remove_all(temp_directory);
         return std::move(chunks_meta);
     }
 
-    void partially_sort(const std::vector<ChunksInfo> &chunks_meta, std::size_t bytes_restriction) {
+    void partially_sort(const std::vector<std::string> &chunks_filenames, std::size_t bytes_restriction) {
         const std::filesystem::path sorted_path = "resources/handled.txt";
         std::fstream sorted_file(sorted_path, std::fstream::out);
         if (!sorted_file.is_open()) {
@@ -93,8 +86,8 @@ namespace custom {
         }
 
         std::vector<std::fstream> chunks_info;
-        std::for_each(chunks_meta.begin(), chunks_meta.end(), [&chunks_info](const ChunksInfo &meta) {
-            std::fstream chunk_file(meta.chunk_name, std::fstream::in);
+        std::for_each(chunks_filenames.begin(), chunks_filenames.end(), [&chunks_info](const std::string &chunk_filename) {
+            std::fstream chunk_file(chunk_filename, std::fstream::in);
             chunks_info.emplace_back(std::move(chunk_file));
         });
 
